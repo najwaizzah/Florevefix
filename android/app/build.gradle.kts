9636c2558bc9644b77bb9f7c109f12f347560927
najwaizzah <najwaizah24@gmail.com>
2025-11-30 12:36:25 +0700

    Update: Fix konflik dependensi map dan upgrade package terkait

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
          applicationId = "com.example.floreveflorist_mobile"
-         minSdk = 21
+         minSdk = flutter.minSdkVersion
          targetSdk = 36
          versionCode = flutter.versionCode
          versionName = flutter.versionName
